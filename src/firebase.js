a4ce36fa3cf3d1f02ffe8b340af8d4af4aec679c
aoki <k.aoki@akatsuki.tokyo>
2023-09-22 19:41:50 +0900

    git

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAoy_bgcYLw_Q_57GP05OzOCSlxMRFxWBI",
-    authDomain: "shelfcomic-d30ef.firebaseapp.com",
-    projectId: "shelfcomic-d30ef",
-    storageBucket: "shelfcomic-d30ef.appspot.com",
-    messagingSenderId: "211040119620",
-    appId: "1:211040119620:web:abe1fb66d4fb9cf086c511"
+    apiKey: "apikey",
+    authDomain: "authDomain",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId"
 };
 
 const app = initializeApp(firebaseConfig);
